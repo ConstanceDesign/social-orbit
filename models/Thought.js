adc8f6b48801a8fb73cdc9a1f01d7beeafaf9aeb
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-02 15:38:48 -0400

    models

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(() => {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes/index.js");
+
+const PORT = 3001 || process.env.PORT;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API Listening on PORT: ${PORT}`);
+  });
+});
