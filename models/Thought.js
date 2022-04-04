d3b4185f2bc57e6a94c06fa49b10349b7a30c5e5
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-03 23:10:43 -0400

    true bug3

--- models/Thought.js
+++ models/Thought.js
@@ -2,33 +2,35 @@ const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
 // Reaction Schema
-const reactionSchema = new Schema({
-  reactionId: [
-    {
+const reactionSchema = new Schema(
+  {
+    reactionId: {
       type: Schema.Types.ObjectId,
-      default: new Types.ObjectId(),
+      default: () => new Types.ObjectId(),
     },
-  ],
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280,
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-  toJSON: {
-    virtuals: true,
-    getters: true,
-  },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // id: false,
+  }
+);
 
 reactionSchema.virtual("ObjectId").get(() => {
   return this._id;
