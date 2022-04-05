23a8faf5ee4bd06edc8db058518c58d178fb5b3c
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-05 16:20:48 -0400

    reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -21,15 +21,7 @@ const thoughtController = {
       .catch((err) => res.json(err));
   },
 
-  // Get all Thoughts
-  // getThought(req, res) {
-  //   Thought.find({})
-  //     .then((dbThoughtData) => res.json(dbThoughtData))
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.sendStatus(400);
-  //     });
-  // },
+  // Get all thoughts
   getAllThought(req, res) {
     Thought.find({})
       .populate({ path: "thought", select: "-__v" })
@@ -42,20 +34,7 @@ const thoughtController = {
       });
   },
 
-  // Get thought by Id
-  // getThoughtById({ params }, res) {
-  //   Thought.findOne({ _id: params.id })
-  //     .populate({
-  //       path: "reactions",
-  //       select: "__v",
-  //     })
-  //     .sort({ _id: -1 })
-  //     .then((dbThoughtData) => res.json(dbThoughtData))
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.sendStatus(400);
-  //     });
-  // },
+  // Get thought
   getThoughtById({ params }, res) {
     Thought.findOne({ _id: params.id })
       .populate({
@@ -111,16 +90,31 @@ const thoughtController = {
     )
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({ message: "No Thought exists with this Id." });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
         res.json(dbThoughtData);
       })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
+      .catch((err) => res.json(err));
   },
+  // addReaction({ body, params }, res) {
+  //   Thought.findOneAndUpdate(
+  //     { _id: params.id },
+  //     { $push: { reaction: body } },
+  //     { new: true, runValidators: true }
+  //   )
+  //     .then((dbThoughtData) => {
+  //       if (!dbThoughtData) {
+  //         res.status(404).json({ message: "No Thought exists with this Id." });
+  //         return;
+  //       }
+  //       res.json(dbThoughtData);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       res.status(400).json(err);
+  //     });
+  // },
 
   // Delete reaction
   deleteReaction({ params }, res) {
