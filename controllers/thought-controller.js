0965bb39006ab46526f0a356ec82b8a1e4f2a457
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-04 21:38:02 -0400

    one more

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,7 +3,7 @@ const { Thought, User } = require("../models");
 const thoughtController = {
   // Create new thought
   createThought(req, res) {
-    Thought.create(req.body)
+    Thought.create(body)
       .then(({ _id }) => {
         return User.findOneAndUpdate(
           { _id: params.userId },
